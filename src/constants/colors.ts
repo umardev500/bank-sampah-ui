50edca110e060a7e0f5718aa55f8b60e054db63e
umar <umar.dev500@gmail.com>
2023-04-07 15:56:27 +0700

    add colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,4 +1,21 @@
+const gray = {
+  gray50: '#F9FAFB',
+  gray100: '#F3F4F6',
+  gray200: '#E5E7EB',
+  gray300: '#D1D5DB',
+  gray400: '#9CA3AF',
+  gray500: '#6B7280',
+  gray600: '#4B5563',
+};
+
+const green = {
+  green500: '#22C55E',
+  green600: '#16A34A',
+};
+
 export const colors = {
   primary: '#6F69AC',
   white: '#fff',
+  gray: gray,
+  green: green,
 };
