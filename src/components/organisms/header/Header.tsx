34aead4eb85b6d1cfba70f9f6d136475cf606745
umar <umar.dev500@gmail.com>
2023-04-11 16:25:53 +0700

    add arrow back icon

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -1,6 +1,6 @@
 import {colors} from 'constants/colors';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 interface Props {
@@ -10,6 +10,10 @@ interface Props {
 export const Header: React.FC<Props> = ({title}) => {
   return (
     <SafeAreaView style={styles.root}>
+      <Image
+        style={styles.arrow}
+        source={require('assets/icons/arrow-back.png')}
+      />
       <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
       </View>
@@ -21,9 +25,17 @@ const styles = StyleSheet.create({
   root: {
     backgroundColor: colors.white,
     elevation: 4,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  arrow: {
+    width: 24,
+    height: 24,
   },
   container: {
-    paddingLeft: 16,
+    paddingLeft: 24,
     paddingRight: 16,
     height: 56,
     flexDirection: 'row',
